8bd9249b9e054ae8bb6fb6f949f2198242ef05f9
Robcio <133985604+robcio2137@users.noreply.github.com>
2023-05-18 16:56:15 +0200

    Dodano kod źródłowy

---
+++ MetalowaRura/Program.cs
@@ -0,0 +1,74 @@
+﻿using MetalowaRura.Properties;
+using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetalowaRura
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.Run(new Rura());
+        }
+    }
+
+
+    public class Rura : ApplicationContext
+    {
+
+        private NotifyIcon trayIcon;
+
+        public Rura()
+        {
+            // Initialize Tray Icon
+            trayIcon = new NotifyIcon()
+            {
+                Icon = Resource1.intel,
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                new MenuItem("Wyjdź", Exit)
+            }),
+                Visible = true
+            };
+
+            // Powiadomienie
+            new ToastContentBuilder()
+.AddText("Metalowa Rura zainicjowana")
+.AddText("Made in robcio2137's basement")
+.Show();
+
+            // METALOWA RURA!!!!
+
+            // Randomowy czas (Milisekundy)
+            Random rnd = new Random();
+            int number = rnd.Next(1000, 6000);
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    System.Threading.Thread.Sleep(number * 60);
+                    System.Media.SoundPlayer player = new System.Media.SoundPlayer(Resource1.Stable);
+                    player.Play();
+                }
+            });
+
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
