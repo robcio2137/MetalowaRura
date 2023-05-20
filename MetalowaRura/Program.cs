52293892d9994747a365fca0c3a03a942577cb80
Robcio <robertszyjkowski@wp.pl>
2023-05-20 09:28:44 +0200

    Poprawki oraz kompatybilność dla 7/8
    Poprawki (zmiana funkcji powiadomienia, poprawienie losowania czasu) oraz kompatybilność dla Windows 7/8

---
+++ MetalowaRura/Program.cs
@@ -0,0 +1,70 @@
+﻿using MetalowaRura.Properties;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetalowaRura
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.Run(new Rura());
+        }
+    }
+
+
+    public class Rura : ApplicationContext
+    {
+
+        private NotifyIcon trayIcon;
+
+        public Rura()
+        {
+            // Initialize Tray Icon
+            // Powiadomienie
+            trayIcon = new NotifyIcon()
+            {
+                Icon = Resource1.intel,
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                    new MenuItem("Wyjdź", Exit)
+                }),
+                Visible = true,
+            };
+            trayIcon.ShowBalloonTip(3000, "Metalowa Rura zainicjowana", "Made in robcio2137's basement.", ToolTipIcon.Warning);
+                // METALOWA RURA!!!!
+            
+
+            Task.Run(() =>
+            {
+                Random rnd = new Random();
+                while (true)
+                {
+                    // Randomowy czas
+                    int number = rnd.Next(1000, 5000);
+
+                    System.Threading.Thread.Sleep(number * 60);
+                    System.Media.SoundPlayer player = new System.Media.SoundPlayer(Resource1.Stable);
+                    player.Play();
+                }
+            });
+
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# MetalowaRura
+## Program do puszczania dźwięku spadającej metalowej rury w losowym momencie.
+### Działa tylko na Windows 7 do 11
+
+
+Po otworzeniu programu co 1-6 minut zostanie odtworzony dźwięk spadającej metalowej rury.
+Jest on ukryty jako proces w tle pod nazwą **"Host środowiska Intel"** oraz jako **ukryta ikona z logiem Intel**.
+Proces można zamknąć przy użyciu Menedżera zadań lub klikając prawym przyciskiem myszy na ukrytą ikonę oraz klikając "Wyjdź".
+
+### Używa licencji: GNU General Public License v3.0
